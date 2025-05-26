b6243dd3a90ef800df09b2448315042dea4988aa
dustin liu <e2755699@gmail.com>
2025-05-27 00:08:12 +0800

    fix translation

--- lib/src/widget/common/global_state.dart
+++ lib/src/widget/common/global_state.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/foundation.dart';
 
-ValueNotifier<int> globalEditModeNotifier = ValueNotifier<int>(1);
-ValueNotifier<int> permissions = ValueNotifier<int>(1); //1:主管 2:測試人員 0:訪客?
+ValueNotifier<int> globalEditModeNotifier = ValueNotifier<int>(0);
+ValueNotifier<int> permissions = ValueNotifier<int>(0); //1:主管 2:測試人員 0:訪客?
